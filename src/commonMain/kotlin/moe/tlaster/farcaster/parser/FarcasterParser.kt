e46a67be78ac3be3964fd3a0a82d52bab5d62195
Tlaster <tlaster@outlook.com>
2024-06-18 17:38:54 +0900

    version 0.1.7

--- src/commonMain/kotlin/moe/tlaster/farcaster/parser/FarcasterParser.kt
+++ src/commonMain/kotlin/moe/tlaster/farcaster/parser/FarcasterParser.kt
@@ -6,11 +6,12 @@ import moe.tlaster.farcaster.parser.tree.Node
 import moe.tlaster.farcaster.parser.tree.TreeBuilder
 
 class FarcasterParser(
+    private val enableDotInUserName: Boolean = false,
     private val customUserSuffix: List<String> = listOf("twitter", "lens", "github", "telegram", "eth"),
 ) {
     fun parse(input: String): List<Node> {
         val tokenizer = Tokenizer(
-            enableDotInUserName = false,
+            enableDotInUserName = enableDotInUserName,
             customUserSuffix = customUserSuffix,
         )
         val tokenCharacterTypes = tokenizer.parse(StringReader(input))
